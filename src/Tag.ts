3587dce5313fb36ae456a368d2b009a5800534e9
Taylor Hanayik <hanayik@gmail.com>
2025-12-02 17:06:06 +0000

    Convert Tag.js to TypeScript with proper type definitions

--- src/Tag.ts
+++ src/Tag.ts
@@ -1,29 +1,43 @@
-import { WriteBufferStream } from "./BufferStream.js";
+import { ReadBufferStream, WriteBufferStream } from "./BufferStream";
 import {
     EXPLICIT_LITTLE_ENDIAN,
     IMPLICIT_LITTLE_ENDIAN,
     SEQUENCE_DELIMITER_TAG,
     SEQUENCE_ITEM_TAG
 } from "./constants/dicom";
-import { ValueRepresentation } from "./ValueRepresentation.js";
+import {
+    ValueRepresentation,
+    type DicomValue,
+    type TransferSyntax,
+    type VRType,
+    type WriteOptions
+} from "./ValueRepresentation";
 
-function paddingLeft(paddingValue, string) {
+function paddingLeft(paddingValue: string, string: string): string {
     return String(paddingValue + string).slice(-paddingValue.length);
 }
 
-let DicomMessage;
+/** Interface for DicomMessage class methods used by Tag */
+interface DicomMessageClass {
+    _normalizeSyntax(syntax: TransferSyntax): TransferSyntax;
+    isEncapsulated(syntax: TransferSyntax): boolean;
+}
+
+let DicomMessage: DicomMessageClass | undefined;
 
 class Tag {
-    constructor(value) {
+    value: number;
+
+    constructor(value: number) {
         this.value = value;
     }
 
     /** Helper method to avoid circular dependencies */
-    static setDicomMessageClass(dicomMessageClass) {
+    static setDicomMessageClass(dicomMessageClass: DicomMessageClass): void {
         DicomMessage = dicomMessageClass;
     }
 
-    toString() {
+    toString(): string {
         return (
             "(" +
             paddingLeft("0000", this.group().toString(16).toUpperCase()) +
@@ -33,71 +47,71 @@ class Tag {
         );
     }
 
-    toCleanString() {
+    toCleanString(): string {
         return (
             paddingLeft("0000", this.group().toString(16).toUpperCase()) +
             paddingLeft("0000", this.element().toString(16).toUpperCase())
         );
     }
 
-    is(t) {
+    is(t: number): boolean {
         return this.value === t;
     }
 
     /**
      * @returns true if the tag is an Item or Delimiter instruction
      */
-    isInstruction() {
+    isInstruction(): boolean {
         return this.group() === 0xfffe;
     }
 
-    group() {
+    group(): number {
         return this.value >>> 16;
     }
 
-    element() {
+    element(): number {
         return this.value & 0xffff;
     }
 
-    isPixelDataTag() {
+    isPixelDataTag(): boolean {
         return this.is(0x7fe00010);
     }
 
-    isPrivateCreator() {
+    isPrivateCreator(): boolean {
         const group = this.group();
         const element = this.element();
         return group % 2 === 1 && element < 0x100 && element > 0x00;
     }
 
-    isMetaInformation() {
+    isMetaInformation(): boolean {
         return this.group() < 0x0008;
     }
 
-    isPrivateValue() {
+    isPrivateValue(): boolean {
         const group = this.group();
         const element = this.element();
         return group % 2 === 1 && element > 0x100;
     }
 
-    static fromString(str) {
-        var group = parseInt(str.substring(0, 4), 16),
-            element = parseInt(str.substring(4), 16);
+    static fromString(str: string): Tag {
+        const group = parseInt(str.substring(0, 4), 16);
+        const element = parseInt(str.substring(4), 16);
         return Tag.fromNumbers(group, element);
     }
 
-    static fromPString(str) {
-        var group = parseInt(str.substring(1, 5), 16),
-            element = parseInt(str.substring(6, 10), 16);
+    static fromPString(str: string): Tag {
+        const group = parseInt(str.substring(1, 5), 16);
+        const element = parseInt(str.substring(6, 10), 16);
         return Tag.fromNumbers(group, element);
     }
 
-    static fromNumbers(group, element) {
+    static fromNumbers(group: number, element: number): Tag {
         return new Tag(((group << 16) | element) >>> 0);
     }
 
-    static readTag(stream) {
-        var group = stream.readUint16(),
-            element = stream.readUint16();
+    static readTag(stream: ReadBufferStream): Tag {
+        const group = stream.readUint16();
+        const element = stream.readUint16();
         return Tag.fromNumbers(group, element);
     }
 
@@ -105,7 +119,7 @@ class Tag {
      * Reads the stream looking for the sequence item tags, returning them
      * as a buffer, and returning null on sequence delimiter tag.
      */
-    static getNextSequenceItemData(stream) {
+    static getNextSequenceItemData(stream: ReadBufferStream): Uint8Array | ArrayBufferLike | null | undefined {
         const nextTag = Tag.readTag(stream);
         if (nextTag.is(SEQUENCE_ITEM_TAG)) {
             const itemLength = stream.readUint32();
@@ -123,13 +137,19 @@ class Tag {
         throw Error("Invalid tag in sequence");
     }
 
-    write(stream, vrType, values, syntax, writeOptions) {
+    write(
+        stream: WriteBufferStream,
+        vrType: VRType,
+        values: DicomValue,
+        syntax: TransferSyntax,
+        writeOptions?: WriteOptions
+    ): number {
         const vr = ValueRepresentation.createByTypeString(vrType);
-        const useSyntax = DicomMessage._normalizeSyntax(syntax);
+        const useSyntax = DicomMessage!._normalizeSyntax(syntax);
 
         const implicit = useSyntax === IMPLICIT_LITTLE_ENDIAN;
         const isLittleEndian = useSyntax === IMPLICIT_LITTLE_ENDIAN || useSyntax === EXPLICIT_LITTLE_ENDIAN;
-        const isEncapsulated = this.isPixelDataTag() && DicomMessage.isEncapsulated(syntax);
+        const isEncapsulated = this.isPixelDataTag() && DicomMessage!.isEncapsulated(syntax);
 
         const oldEndian = stream.isLittleEndian;
         stream.setEndian(isLittleEndian);
@@ -137,22 +157,27 @@ class Tag {
         stream.writeUint16(this.group());
         stream.writeUint16(this.element());
 
-        var tagStream = new WriteBufferStream(256),
-            valueLength;
+        const tagStream = new WriteBufferStream(256);
+        let valueLength: number;
         tagStream.setEndian(isLittleEndian);
 
+        // VR subclasses have different writeBytes signatures - use polymorphic call
+        const vrWithWriteBytes = vr as unknown as {
+            writeBytes(stream: WriteBufferStream, value: DicomValue, ...args: (TransferSyntax | boolean | WriteOptions | undefined)[]): number;
+        };
+
         if (vrType === "OW" || vrType === "OB" || vrType === "UN") {
-            valueLength = vr.writeBytes(tagStream, values, useSyntax, isEncapsulated, writeOptions);
+            valueLength = vrWithWriteBytes.writeBytes(tagStream, values, useSyntax, isEncapsulated, writeOptions);
         } else if (vrType === "SQ") {
-            valueLength = vr.writeBytes(tagStream, values, useSyntax, writeOptions);
+            valueLength = vrWithWriteBytes.writeBytes(tagStream, values, useSyntax, writeOptions);
         } else {
-            valueLength = vr.writeBytes(tagStream, values, writeOptions);
+            valueLength = vrWithWriteBytes.writeBytes(tagStream, values, writeOptions);
         }
 
         if (vrType === "SQ") {
             valueLength = 0xffffffff;
         }
-        var written = tagStream.size + 4;
+        let written = tagStream.size + 4;
 
         if (implicit) {
             stream.writeUint32(valueLength);
